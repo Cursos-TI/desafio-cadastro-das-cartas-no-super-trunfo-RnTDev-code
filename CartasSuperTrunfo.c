9243f1da1f545ac7015154c4e9a6a94914e66809
RnTDev-code <rntindev@gmail.com>
2025-08-11 13:11:42 +0000

    super Trunfo LVL mestre. removido os coments para ficar limpo o codigo.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,10 +5,10 @@ struct Carta {
     char NomeDaCidade[25];
     char LetraDoEstado[15];
     char NumeroDaCarta[10];
-    int populacao;
+    unsigned long int populacao; 
     float area;
     float pib;
-    int pontos_turisticos;
+    int PontosTuristicos;
 };
 
 int main() {
@@ -16,14 +16,14 @@ int main() {
     struct Carta carta1;
     struct Carta carta2;
 
-    // Novas variáveis para armazenar os resultados dos cálculos
-    // aqui fiquei na duvida se eu deveria adicionaria junto das outras variaveis na struct e fui pesquisar pra entender e aprender
-    // aprendi sobre dados fontes e dados calculados ;)
-    float densidadePopulacional1, pibPerCapita1;
-    float densidadePopulacional2, pibPerCapita2;
-     
-    //quis deixar mais legal usando o '*' (aprendi com o prof Sergio).
-    printf(" *** Cadastro da Primeira Carta ***  \n");
+    float densidadePopulacional1, pibPerCapita1, superPoder1;
+    float densidadePopulacional2, pibPerCapita2, superPoder2;
+
+    int Carta1VenceuPopulacao, Carta1VenceuArea, Carta1VenceuPIB, Carta1VenceuPontosTuristicos;
+    int Carta1VenceuDensidade, Carta1VenceuPIBperCapta, Carta1VenceuSuperPoder;
+
+
+    printf("--- Cadastro da Primeira Carta ---\n");
     
     printf("Digite o codigo da carta (ex: A01): ");
     scanf("%s", carta1.CodigoDaCarta);
@@ -38,7 +38,7 @@ int main() {
     scanf("%s", carta1.NumeroDaCarta);
 
     printf("Digite a populacao: ");
-    scanf("%d", &carta1.populacao);
+    scanf("%lu", &carta1.populacao); 
 
     printf("Digite a area (em km²): ");
     scanf("%f", &carta1.area);
@@ -46,16 +46,12 @@ int main() {
     printf("Digite o PIB (em bilhoes): ");
     scanf("%f", &carta1.pib);
 
-    printf("Digite o numero de pontos turisticos: ");
-    scanf("%d", &carta1.pontos_turisticos);
-
-    // Aqui os calculos da carta 1
-    densidadePopulacional1 = (float)carta1.populacao / carta1.area;
-    pibPerCapita1 = (carta1.pib * 1000000000) / (float)carta1.populacao; // Convertendo PIB para valor real (bati cabeça pra entender e aprender isso aqui ;( )
+    printf("Digite o numero de Pontos Turisticos: ");
+    scanf("%d", &carta1.PontosTuristicos);
     
-    printf(" \n");
+    printf("\n");
 
-    printf(" *** Cadastro da Segunda Carta ***  \n");
+    printf("--- Cadastro da Segunda Carta ---\n");
 
     printf("Digite o codigo da carta (ex: B04): ");
     scanf("%s", carta2.CodigoDaCarta);
@@ -70,7 +66,7 @@ int main() {
     scanf("%s", carta2.NumeroDaCarta);
 
     printf("Digite a populacao: ");
-    scanf("%d", &carta2.populacao);
+    scanf("%lu", &carta2.populacao);
 
     printf("Digite a area (em km²): ");
     scanf("%f", &carta2.area);
@@ -78,35 +74,33 @@ int main() {
     printf("Digite o PIB (em bilhoes): ");
     scanf("%f", &carta2.pib);
 
-    printf("Digite o numero de pontos turisticos: ");
-    scanf("%d", &carta2.pontos_turisticos);
+    printf("Digite o numero de Pontos Turisticos: ");
+    scanf("%d", &carta2.PontosTuristicos);
 
-      // Aqui os calculos da carta 2
+    densidadePopulacional1 = (float)carta1.populacao / carta1.area;
+    pibPerCapita1 = (carta1.pib * 1000000000) / (float)carta1.populacao;
+    superPoder1 = (float)carta1.populacao + carta1.area + carta1.pib + (float)carta1.PontosTuristicos + pibPerCapita1 + (1.0 / densidadePopulacional1);
+
     densidadePopulacional2 = (float)carta2.populacao / carta2.area;
-    pibPerCapita2 = (carta2.pib * 1000000000) / (float)carta2.populacao; 
+    pibPerCapita2 = (carta2.pib * 1000000000) / (float)carta2.populacao;
+    superPoder2 = (float)carta2.populacao + carta2.area + carta2.pib + (float)carta2.PontosTuristicos + pibPerCapita2 + (1.0 / densidadePopulacional2);
 
-    //quis deixar mais legal usando o '=' (aprendi com o prof Sergio).
-    printf(" \n ====Dados da Primeira Carta==== \n");
-    printf("Codigo da Carta: %s \n", carta1.CodigoDaCarta);
-    printf("Nome da Cidade: %s \n", carta1.NomeDaCidade);
-    printf("Populacao: %d habitantes \n", carta1.populacao);
-    printf("Area: %.2f km² \n", carta1.area);
-    printf("PIB: R$ %.2f bilhoes \n", carta1.pib);
-    printf("Pontos Turisticos: %d \n", carta1.pontos_turisticos);
+    Carta1VenceuPopulacao = carta1.populacao > carta2.populacao;
+    Carta1VenceuArea = carta1.area > carta2.area;
+    Carta1VenceuPIB = carta1.pib > carta2.pib;
+    Carta1VenceuPontosTuristicos = carta1.PontosTuristicos > carta2.PontosTuristicos;
+    Carta1VenceuPIBperCapta = pibPerCapita1 > pibPerCapita2;
+    Carta1VenceuSuperPoder = superPoder1 > superPoder2;
+    Carta1VenceuDensidade = densidadePopulacional1 < densidadePopulacional2;
 
-    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacional1);
-    printf("PIB per Capita: R$ %.2f\n", pibPerCapita1);
-
-    printf(" \n ==== Dados da Segunda Carta ==== \n");
-    printf("Codigo da Carta: %s \n", carta2.CodigoDaCarta);
-    printf("Nome da Cidade: %s \n", carta2.NomeDaCidade);
-    printf("Populacao: %d habitantes \n", carta2.populacao);
-    printf("Area: %.2f km² \n", carta2.area);
-    printf("PIB: R$ %.2f bilhoes \n", carta2.pib);
-    printf("Pontos Turisticos: %d \n", carta2.pontos_turisticos);
-
-    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacional2);
-    printf("PIB per Capita: R$ %.2f\n", pibPerCapita2);
+    printf("\n \n --- Comparacao de Cartas --- \n");
+    printf("Populacao: Carta %d venceu (%d) \n", (Carta1VenceuPopulacao ? 1 : 2), Carta1VenceuPopulacao);
+    printf("Area: Carta %d venceu (%d) \n", (Carta1VenceuArea ? 1 : 2), Carta1VenceuArea);
+    printf("PIB: Carta %d venceu (%d)\n", (Carta1VenceuPIB ? 1 : 2), Carta1VenceuPIB);
+    printf("Pontos Turisticos: Carta %d venceu (%d) \n", (Carta1VenceuPontosTuristicos ? 1 : 2), Carta1VenceuPontosTuristicos);
+    printf("Densidade Populacional: Carta %d venceu (%d) \n", (Carta1VenceuDensidade ? 1 : 2), Carta1VenceuDensidade);
+    printf("PIB per Capita: Carta %d venceu (%d) \n", (Carta1VenceuPIBperCapta ? 1 : 2), Carta1VenceuPIBperCapta);
+    printf("Super Poder: Carta %d venceu (%d) \n", (Carta1VenceuSuperPoder ? 1 : 2), Carta1VenceuSuperPoder);
 
     return 0;
 }
